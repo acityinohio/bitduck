b337497a4b02115b8e39162f93f52c804a6f3ad5
Josh Cincinnati <acityinohio@users.noreply.github.com>
2015-02-27 13:23:21 -0500

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bitduck
+
+Building an ephemeral Go (Baduk) webapp, using Go, with a library I built with Go. Nothing works right now. Will add some neat bitcoin related features using BlockCypher's API shortly!
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*"))
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "index.html", "")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
